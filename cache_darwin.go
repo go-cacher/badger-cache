028b9557c9f9bac467089d3dca63a5f75adb1e23
godcong is not god <jumbycc@163.com>
2020-04-09 14:49:47 +0800

    Create cache_darwin.go

---
+++ cache_darwin.go
@@ -0,0 +1,18 @@
+  
+package cache
+
+import (
+	"github.com/dgraph-io/badger/v2"
+	"github.com/gocacher/cacher"
+)
+
+func newBadgerCache(path string) cacher.Cacher {
+	db, err := badger.Open(badger.DefaultOptions(path))
+	if err != nil {
+		panic(err)
+	}
+	return &BadgerCache{
+		path: path,
+		db:   db,
+	}
+}
